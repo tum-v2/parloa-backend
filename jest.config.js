ec7975b661f0054fec0eab8e250d992e97db637a
Ozan Aydın <ozan.aydin@precipoint.de>
2023-11-08 00:26:18 +0100

    Add Jest as the testing framework for unit tests.

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line tsdoc/syntax
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+// eslint-disable-next-line no-undef
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+};
