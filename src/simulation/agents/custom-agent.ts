ef7bc2a1531f8f976e27bf1435840cde3a3ceda7
Ivan Stoyanov <ge36qij@mytum.de>
2023-11-12 20:30:56 +0200

    Adapted implementation of the user agent

--- src/simulation/agents/custom-agent.ts
+++ src/simulation/agents/custom-agent.ts
@@ -1,262 +1,293 @@
-import { appendFileSync } from 'fs';
 import { BaseChatModel } from 'langchain/chat_models/base';
-import { BaseMessage } from 'langchain/schema';
-import { AgentConfig, RestAPITool } from './agent-config';
 import { HumanMessagePromptTemplate, SystemMessagePromptTemplate } from 'langchain/prompts';
+import { BaseMessage } from 'langchain/schema';
+import { CustomAgentConfig, RestAPITool, RouteToCoreTool } from './custom-agent-config';
+import moment from 'moment';
+import { appendFileSync } from 'fs';
 
-interface MsgHistoryItem {
-  lc_msg: BaseMessage;
-  type: 'human_input' | 'system_prompt' | 'tool_call' | 'tool_output' | 'msg_to_user' | 'route';
+const USER_MESSAGE_ACTION: string = 'message_to_user';
+
+type MsgTypes = 'human_input' | 'system_prompt' | 'tool_call' | 'tool_output' | 'msg_to_user' | 'route';
+
+class MsgHistoryItem {
+  lcMsg: BaseMessage;
+  type: MsgTypes;
   timestamp: Date;
-  user_input?: string | null;
-  msg_to_user?: string | null;
-  intermediate_msg?: string | null;
-  action?: string | null;
-  tool_input?: Record<string, any> | null;
-  tool_output?: any | null;
-  parent_id?: string | null;
+  userInput: string | null;
+  msgToUser: string | null;
+  intermediateMsg: string | null;
+  action: string | null;
+  toolInput: Record<string, any> | null;
+  toolOutput: any | null;
+  parentId: string | null;
+
+  constructor(
+    lcMsg: BaseMessage,
+    type: MsgTypes,
+    userInput?: string,
+    msgToUser?: string,
+    intermediateMsg?: string,
+    action?: string,
+    toolInput?: Record<string, any>,
+    toolOutput?: any,
+    parentId?: string,
+  ) {
+    this.lcMsg = lcMsg;
+    this.type = type;
+    this.timestamp = new Date();
+    this.userInput = userInput || null;
+    this.msgToUser = msgToUser || null;
+    this.intermediateMsg = intermediateMsg || null;
+    this.action = action || null;
+    this.toolInput = toolInput || null;
+    this.toolOutput = toolOutput || null;
+    this.parentId = parentId || null;
+  }
 }
 
-class Colors {
-  static RED = '\x1b[91m';
-  static GREEN = '\x1b[92m';
-  static YELLOW = '\x1b[93m';
-  static BLUE = '\x1b[94m';
-  static MAGENTA = '\x1b[95m';
-  static CYAN = '\x1b[96m';
-  static GREY = '\x1b[90m';
-  static END = '\x1b[0m';
+enum Colors {
+  RED = '\u001b[91m',
+  GREEN = '\u001b[92m',
+  YELLOW = '\u001b[93m',
+  BLUE = '\u001b[94m',
+  MAGENTA = '\u001b[95m',
+  CYAN = '\u001b[96m',
+  GREY = '\u001b[90m',
+  END = '\u001b[0m',
 }
 
-type MessageCallback = (customAgent: CustomAgent, msgHistoryItem: MsgHistoryItem) => Promise<void>;
+type Callback = (agent: CustomAgent, historyItem: MsgHistoryItem) => Promise<void>;
 
-export class CustomAgent {
+type CombinedTool = RestAPITool | RouteToCoreTool;
+
+type ParamType = {
+  description: string;
+  type: string;
+};
+
+type ToolDescription = {
+  description: string;
+  output?: string;
+};
+
+class CustomAgent {
   chat_model: BaseChatModel;
-  config: AgentConfig;
-  prompt_log_file_path: string | null;
-  chat_log_file_path: string | null;
-  is_verbose: boolean;
-  is_echo_human_input: boolean;
-  message_callback: MessageCallback | null;
-  message_history: MsgHistoryItem[];
+  config: CustomAgentConfig;
+  promptLogFilePath: string | null;
+  chatLogFilePath: string | null;
+  isVerbose: boolean;
+  isEchoHumanInput: boolean;
+  messageCallback: Callback | null = null;
+  combinedTools: { [key: string]: CombinedTool };
+  messageHistory: MsgHistoryItem[];
 
   constructor(
     chat_model: BaseChatModel,
-    config: AgentConfig,
-    prompt_log_file_path: string | null,
-    chat_log_file_path: string | null,
-    is_verbose: boolean = true,
-    is_echo_human_input: boolean = false,
-    message_callback: MessageCallback | null = null,
+    config: CustomAgentConfig,
+    promptLogFilePath?: string | null,
+    chatLogFilePath?: string | null,
+    isVerbose: boolean = true,
+    isEchoHumanInput: boolean = false,
+    messageCallback?: Callback,
   ) {
     this.chat_model = chat_model;
     this.config = config;
-    this.prompt_log_file_path = prompt_log_file_path;
-    this.chat_log_file_path = chat_log_file_path;
-    this.is_verbose = is_verbose;
-    this.is_echo_human_input = is_echo_human_input;
-    this.message_callback = message_callback;
-    this.message_history = [];
+    this.promptLogFilePath = promptLogFilePath || null;
+    this.chatLogFilePath = chatLogFilePath || null;
+    this.isVerbose = isVerbose;
+    this.isEchoHumanInput = isEchoHumanInput;
+    this.messageCallback = messageCallback || null;
+    this.combinedTools = {
+      ...this.config.restApiTools,
+      ...this.config.routingTools,
+    };
+    this.messageHistory = [];
   }
 
   async startAgent(): Promise<string> {
-    await this.addMessage({
-      type: 'system_prompt',
-      lc_msg: this.getSystemPrompt(),
-      timestamp: new Date(),
-    });
+    /*Initializes the messages with the starting system prompt and returns the welcome message.
+    No LLM call is made.
+    Logs if log files set and prints if isVerbose for the class instance"""*/
 
-    return this.config.welcome_message;
+    const lcMsg: BaseMessage = await this.getSystemPrompt();
+    await this.addMessage(new MsgHistoryItem(lcMsg, 'system_prompt'));
+
+    return this.config.welcomeMessage;
   }
 
-  async processHumanInput(human_input: string, id: string | null = null): Promise<string> {
-    await this.addMessage({
-      type: 'human_input',
-      lc_msg: this.getHumanPrompt(human_input),
-      user_input: human_input,
-      parent_id: id,
-      timestamp: new Date(),
-    });
+  async processHumanInput(humanInput: string, id: string | null = null): Promise<string> {
+    const lcMsg: BaseMessage = await this.getHumanPrompt(humanInput);
+    await this.addMessage(new MsgHistoryItem(lcMsg, 'human_input', humanInput, id ?? undefined));
 
     let response: Record<string, any> = {};
     let action: string | null = null;
-    let action_input: string | Record<string, any> | null = null;
-    let api_tool_config: RestAPITool | null = null;
+    let actionInput: string | Record<string, any> | null = null;
+    let apiToolConfig: RestAPITool | null = null;
 
-    // Call tools until we get a USER_MESSAGE_ACTION or an action which is not a rest_api_tool
+    //call tools  until we get a USER_MESSAGE_ACTION or an action which is not rest_apit_tool
     for (;;) {
-      const responseMessage = await this.chat_model.call(this.message_history.map((item) => item.lc_msg));
-
+      const responseMessage = await this.chat_model.call(this.messageHistory.map((msg) => msg.lcMsg));
       response = JSON.parse(responseMessage.content.toString());
-      action = response.action;
-      action_input = response.action_input || {};
+      action = response.get('action');
+      actionInput = response.get('action_input', {});
 
-      if (action === 'message_to_user') {
-        await this.addMessage({
-          type: 'msg_to_user',
-          msg_to_user: String(action_input),
-          lc_msg: responseMessage,
-          parent_id: id,
-          timestamp: new Date(),
-        });
+      if (action === USER_MESSAGE_ACTION) {
+        await this.addMessage(
+          new MsgHistoryItem(
+            responseMessage,
+            'msg_to_user',
+            undefined,
+            String(actionInput),
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            id!,
+          ),
+        );
         break;
       }
 
-      if (typeof action_input !== 'object') {
+      if (typeof actionInput !== 'object') {
         throw new Error(`ERROR: Invalid action_input in response: ${JSON.stringify(response, null, 4)}`);
       }
 
-      if (action && this.config.routing_tools[action]) {
-        await this.addMessage({
-          type: 'route',
-          action,
-          tool_input: action_input,
-          intermediate_msg: response.intermediate_message,
-          lc_msg: responseMessage,
-          parent_id: id,
-          timestamp: new Date(),
-        });
+      if (action && this.config.routingTools[action]) {
+        await this.addMessage(
+          new MsgHistoryItem(
+            responseMessage,
+            'route',
+            undefined,
+            undefined,
+            response.intermediate_message,
+            action,
+            actionInput!,
+            undefined,
+            id!,
+          ),
+        );
         break;
       }
 
       // It's API tool time!
-      if (action && !(action in this.config.rest_api_tools)) {
+      if (action && !(action in this.config.restApiTools)) {
         throw new Error(`ERROR: Missing or invalid tool in response action: ${JSON.stringify(response, null, 4)}`);
       }
 
       if (action) {
-        api_tool_config = this.config.rest_api_tools[action];
+        apiToolConfig = this.config.restApiTools[action];
 
-        await this.addMessage({
-          type: 'tool_call',
-          action,
-          tool_input: action_input,
-          intermediate_msg: response.intermediate_message,
-          lc_msg: responseMessage,
-          parent_id: id,
-          timestamp: new Date(),
-        });
+        await this.addMessage(
+          new MsgHistoryItem(
+            responseMessage,
+            'tool_call',
+            undefined,
+            undefined,
+            response.intermediate_message,
+            action,
+            actionInput!,
+            undefined,
+            id!,
+          ),
+        );
 
-        const toolOutput = api_tool_config.func(action_input);
-        await this.addMessage({
-          type: 'tool_output',
-          action,
-          tool_output: toolOutput,
-          lc_msg: this.getToolOutputPrompt(action, toolOutput),
-          parent_id: id,
-          timestamp: new Date(),
-        });
+        const toolOutput = apiToolConfig.func(actionInput);
+        const lcMsg = await this.getToolOutputPrompt(action, toolOutput);
+        await this.addMessage(
+          new MsgHistoryItem(lcMsg, 'tool_output', undefined, undefined, undefined, action, undefined, toolOutput, id!),
+        );
       }
     }
-
-    return String(action_input); // type: ignore[reportGeneralTypeIssues] -- we raise in while loop but Pylance doesn't spot
+    return String(actionInput); // type: ignore[reportGeneralTypeIssues] -- we raise in while loop but Pylance doesn't spot
   }
 
-  private getSystemPrompt(): any {
-    const toolDescriptions: Record<string, Record<string, any>> = {};
-    for (const [toolName, tool] of Object.entries({
-      ...this.config.rest_api_tools,
-      ...this.config.routing_tools,
-    })) {
-      if (tool.is_active) {
+  getSystemPrompt() {
+    const toolDescriptions: Record<string, ToolDescription> = {};
+    for (const toolName in this.combinedTools) {
+      const tool = this.combinedTools[toolName];
+      if (tool.isActive) {
         toolDescriptions[toolName] = { description: tool.description };
 
-        const inputs: Record<string, any> = {};
-        for (const param of tool.request.parameters) {
-          if (param.expected_from_llm) {
-            const paramName = param.name_for_prompt || param.name;
+        const inputs: Record<string, ParamType> = {};
+        tool.request.parameters.forEach((param) => {
+          const param_name = param.nameForPrompt ? param.nameForPrompt : param.name;
 
-            inputs[paramName] = {
-              description: param.description,
-              type: param.type,
-            };
+          inputs[param_name] = {
+            description: param.description,
+            type: param.type,
+          };
+
+          let output: string | null = null;
+
+          if (tool instanceof RestAPITool) {
+            output = tool.response.outputDescription;
           }
-        }
-        if (Object.keys(inputs).length > 0) {
-          toolDescriptions[toolName].input = inputs;
-        }
 
-        const output = (tool as any).response?.output_description || null;
-        if (output) {
-          toolDescriptions[toolName].output = output;
-        }
+          if (output) {
+            toolDescriptions[toolName].output = output;
+          }
+        });
       }
     }
-
-    return SystemMessagePromptTemplate.fromTemplate(this.config.system_prompt_template).format({
+    return SystemMessagePromptTemplate.fromTemplate(this.config.systemPromptTemplate).format({
       role: this.config.role,
       persona: this.config.persona,
-      conversation_strategy: this.config.conversation_strategy,
+      conversation: this.config.conversationStrategy,
       tasks: JSON.stringify(this.config.tasks, null, 2),
-      current_date: new Date().toLocaleDateString('en-US', {
-        day: '2-digit',
-        month: 'short',
-        year: 'numeric',
-      }),
+      current_date: moment().format('YYYY-MM-DD'),
       formatted_tools: JSON.stringify(toolDescriptions, null, 2),
     });
   }
 
-  private getHumanPrompt(humanInput: string): any {
-    return HumanMessagePromptTemplate.fromTemplate(this.config.human_input_template).format({
+  getHumanPrompt(humanInput: string) {
+    return HumanMessagePromptTemplate.fromTemplate(this.config.humanInputTemplate).format({
       human_input: humanInput,
     });
   }
 
-  private getToolOutputPrompt(toolName: string, toolOutput: any): any {
-    return HumanMessagePromptTemplate.fromTemplate(this.config.tool_output_template).format({
+  getToolOutputPrompt(toolName: string, toolOutput: string) {
+    return HumanMessagePromptTemplate.fromTemplate(this.config.humanInputTemplate).format({
       tool_output: toolOutput,
       tool_name: toolName,
     });
   }
 
-  private async addMessage(newMessage: MsgHistoryItem): Promise<void> {
-    this.message_history.push(newMessage);
+  async addMessage(newMessage: MsgHistoryItem): Promise<void> {
+    this.messageHistory.push(newMessage);
     this.logMessage(newMessage);
 
-    if (this.message_callback) {
-      await this.message_callback(this, newMessage);
+    if (this.messageCallback) {
+      await this.messageCallback(this, newMessage);
     }
   }
 
-  private logMessage(msg: MsgHistoryItem): void {
-    switch (msg.type) {
-      case 'system_prompt':
-        this.logChat(`🤖 ${Colors.BLUE}${this.config.welcome_message}${Colors.END}`);
-        break;
-      case 'human_input':
-        this.logChat(`${Colors.GREEN}👧 ${msg.user_input}${Colors.END}`, this.is_echo_human_input);
-        break;
-      case 'tool_call':
-        this.logChat(`   ⏳ ${Colors.BLUE}${msg.intermediate_msg}${Colors.END}`);
-        this.logChat(
-          `      🛠️ ${Colors.GREY}[${msg.action}] call input: ${JSON.stringify(msg.tool_input)}${Colors.END}`,
-        );
-        break;
-      case 'tool_output':
-        this.logChat(`      🛠️ ${Colors.GREY}[${msg.action}] result: ${JSON.stringify(msg.tool_output)}${Colors.END}`);
-        break;
-      case 'msg_to_user':
-        this.logChat(`🤖 ${Colors.BLUE}${msg.msg_to_user}${Colors.END}`);
-        break;
-      case 'route':
-        this.logChat(`   ⏳ ${Colors.BLUE}${msg.intermediate_msg}${Colors.END}`);
-        this.logChat(`️️️️🏓 ${Colors.BLUE}${msg.action} ${Colors.GREY}${JSON.stringify(msg.tool_input)}${Colors.END}`);
-        break;
+  logMessage(msg: MsgHistoryItem) {
+    if (msg.type === 'system_prompt') {
+      this.logChat(`🤖 ${Colors.BLUE}${this.config.welcomeMessage}${Colors.END}`);
+    } else if (msg.type === 'human_input') {
+      this.logChat(`${Colors.GREEN}👧 ${msg.userInput}${Colors.END}`, this.isEchoHumanInput);
+    } else if (msg.type === 'tool_call') {
+      this.logChat(`      🛠️ ${Colors.GREY}[${msg.action}] call input: ${msg.toolInput}${Colors.END}`);
+    } else if (msg.type === 'tool_output') {
+      this.logChat(`      🛠️ ${Colors.GREY}[${msg.action}] result: ${msg.toolOutput}${Colors.END}`);
+    } else if (msg.type === 'msg_to_user') {
+      this.logChat(`🤖 ${Colors.BLUE}${msg.msgToUser}${Colors.END}`);
+    } else if (msg.type === 'route') {
+      this.logChat(`   ⏳ ${Colors.BLUE}${msg.intermediateMsg}${Colors.END}`);
+      this.logChat(`🏓 ${Colors.BLUE}${msg.action} ${Colors.GREY}${msg.toolInput}${Colors.END}`);
     }
 
-    if (this.prompt_log_file_path) {
-      appendFileSync(this.prompt_log_file_path, msg.lc_msg.content.toString());
+    if (this.promptLogFilePath) {
+      appendFileSync(this.promptLogFilePath, msg.lcMsg.content + '\n');
     }
   }
 
-  private logChat(output: string, isPrint: boolean = true): void {
-    if (this.chat_log_file_path) {
-      const logItem = output.replace(/\\x1B\[\d+(;\d+)*m/g, ''); // Remove color codes
-      appendFileSync(this.chat_log_file_path, logItem + '\n');
+  logChat(output: string, is_print: boolean = true) {
+    if (this.chatLogFilePath) {
+      const logItem = output.replace(/\\x1B\[\d+(;\d+)*m/g, '');
+      appendFileSync(this.chatLogFilePath, logItem + '\n');
     }
-
-    if (this.is_verbose && isPrint) {
+    if (is_print && this.isVerbose) {
       console.log(`${Colors.END}${output}`);
     }
   }
--- src/simulation/agents/user-agent.ts
+++ src/simulation/agents/user-agent.ts
@@ -1,5 +1,5 @@
 // User agent
-import { AgentConfig, defaultAgentConfig } from './agent-config';
+import { CustomAgentConfig } from './custom-agent-config';
 
 const PERSONAS: Record<string, string> = {
   sarcastic:
@@ -21,74 +21,76 @@ const PERSONAS: Record<string, string> = {
     '- Never repeat the same information twice unless explicitly asked.',
 };
 
+const TEMPERATURE = 1;
+
+const ROLE = `You are a human calling a call center trying to change your flight booking.`;
+
+const CONVERSATION_STRATEGY = `- Your primary objective is to change your existing booking.
+- Your booking number is PARL0A.
+- Your name is Claudio Diniz.
+- Your flight date for your booking is November, 12th, 2023. 11am.
+- Your flight is from New York to Boston.f
+- Your booking is for 3 people.
+- You want to change it to November 17 or 18 or 19, ideally 2pm but any time is fine.
+- Generate your responses in a way which is realistic to a phone conversation.
+- Include common voice to text transcription errors in the text.
+- When you successfully changed your booking return { "action": "message_to_user", "action_input": "/hangup"}
+`;
+
+const SYSTEM_PROMPT_TEMPLATE = `# YOUR ROLE
+{role}
+Today's date is {current_date}.
+
+# YOUR PERSONA
+{persona}
+
+# CONVERSATION STRATEGY
+{conversation_strategy}
+
+# TOOLS
+You should gather input from the agent to call tools when a tool is required.
+You have access to the following tools:
+
+{formatted_tools}
+
+{tasks}
+
+# YOUR RESPONSE
+{{
+"tought": < Take a deep breath and think step by step. First include your thoughts based on the last message from the user and consider the full converstation history>
+"action":  <a single action you decided to take next. The action should be either the name of a TOOL name or message_to_user  >
+"action_input": <either all the inputs required for the tool which you gathered from the agent previously or the message to the agent.>
+"intermediate_message": <when calling a tool you should generate a short intermediate message to the user saying to wait a bit because you are doing something>
+}}
+
+Begin! Reminder to ALWAYS respond with a single valid json blob with a single action. Use available tools if necessary.`;
+
+const HUMAN_INPUT_TEMPLATE = `AGENT: {human_input}
+# YOUR RESPONSE
+`;
+const TOOL_OUTPUT_TEMPLATE = `# RESULT FROM '{tool_name}' TOOL
+{tool_output}
+
+# YOUR RESPONSE
+`;
+
 /**
  * Returns the config for the user agent
  * @param persona - One of the follwing strings: "sarcastic" | "nonative" | "terse" | "riddling" | "concise"
  * @returns the configuration for this persona
  */
-export function getSimConfig(persona: string): AgentConfig {
-  const personaConfig = humanSimConfig;
+export function getSimConfig(persona: string): CustomAgentConfig {
   if (!(persona in PERSONAS)) {
     throw new Error(`Persona ${persona} not found. Available personas: ${Object.keys(PERSONAS)}`);
   }
-
+  const personaConfig = new CustomAgentConfig();
+  personaConfig.temperature = TEMPERATURE;
+  personaConfig.role = ROLE;
   personaConfig.persona = PERSONAS[persona];
+  personaConfig.conversationStrategy = CONVERSATION_STRATEGY;
+  personaConfig.systemPromptTemplate = SYSTEM_PROMPT_TEMPLATE;
+  personaConfig.humanInputTemplate = HUMAN_INPUT_TEMPLATE;
+  personaConfig.toolOutputTemplate = TOOL_OUTPUT_TEMPLATE;
 
   return personaConfig;
 }
-const humanSimConfig: AgentConfig = {
-  ...defaultAgentConfig,
-  temperature: 1,
-  welcome_message: '',
-  role: `You are a human calling a call center trying to change your flight booking.`,
-  persona: '',
-  conversation_strategy: `- Your primary objective is to change your existing booking.
-    - Your booking number is PARL0A.
-    - Your name is Claudio Diniz.
-    - Your flight date for your booking is November, 12th, 2023. 11am.
-    - Your flight is from New York to Boston.f
-    - Your booking is for 3 people.
-    - You want to change it to November 17 or 18 or 19, ideally 2pm but any time is fine.
-    - Generate your responses in a way which is realistic to a phone conversation.
-    - Include common voice to text transcription errors in the text.
-    - When you successfully changed your booking return { "action": "message_to_user", "action_input": "/hangup"}
-    `,
-  rest_api_tools: {},
-  routing_tools: {},
-  system_prompt_template: `# YOUR ROLE
-    {role}
-    Today's date is {current_date}.
-    
-    # YOUR PERSONA
-    {persona}
-    
-    # CONVERSATION STRATEGY
-    {conversation_strategy}
-    
-    # TOOLS
-    You should gather input from the agent to call tools when a tool is required.
-    You have access to the following tools:
-    
-    {formatted_tools}
-    
-    {tasks}
-    
-    # YOUR RESPONSE
-    {{
-    "tought": < Take a deep breath and think step by step. First include your thoughts based on the last message from the user and consider the full converstation history>
-    "action":  <a single action you decided to take next. The action should be either the name of a TOOL name or message_to_user  >
-    "action_input": <either all the inputs required for the tool which you gathered from the agent previously or the message to the agent.>
-    "intermediate_message": <when calling a tool you should generate a short intermediate message to the user saying to wait a bit because you are doing something>
-    }}
-    
-    Begin! Reminder to ALWAYS respond with a single valid json blob with a single action. Use available tools if necessary.`,
-  human_input_template: `AGENT: {human_input}
-    # YOUR RESPONSE
-    `,
-  tool_output_template: `# RESULT FROM '{tool_name}' TOOL
-    {tool_output}
-    
-    # YOUR RESPONSE
-    `,
-};
-
