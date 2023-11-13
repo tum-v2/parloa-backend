aeb0a8273a5f493cbfbd7e74502f0e8e96773049
ge42ruv <ge42ruv@mytum.de>
2023-11-13 20:28:44 +0100

    Convert Flight Booking Agent

--- src/simulation/agents/service/service-agent-flight-booker.ts
+++ src/simulation/agents/service/service-agent-flight-booker.ts
@@ -1,1 +1,191 @@
-// A service agent that books flights
+import {
+  APIParam,
+  APIRequest,
+  APIResponse,
+  CustomAgentConfig,
+  RestAPITool,
+  RouteToCoreTool,
+} from '../custom-agent-config';
+
+const bookingNumberParam = new APIParam(
+  'booking_number',
+  'The booking number. Format is 6 alphanumeric characters.',
+  'string',
+);
+
+const authTokenParam = new APIParam('auth_token', 'auth_token returned by the auth tool', 'string');
+
+const auth = () => {};
+const booking_info = () => {};
+const check_availability = () => {};
+const change_flight_date = () => {};
+const get_faq_answer = () => {};
+
+const restApiTools: Record<string, RestAPITool> = {
+  auth: new RestAPITool(
+    'Authenticates a user',
+    new APIRequest([
+      bookingNumberParam,
+      new APIParam('last_name', 'Last Name of the person who made the booking.', 'string'),
+    ]),
+    new APIResponse('auth_token to be used for other tools'),
+    auth,
+  ),
+  booking_info: new RestAPITool(
+    'Retrieves booking details',
+    new APIRequest([authTokenParam, bookingNumberParam]),
+    new APIResponse(
+      '"flight number, departure and arrival airports, departure and arrival times, and the date of the flight"',
+    ),
+    booking_info,
+  ),
+  check_availability: new RestAPITool(
+    'Returns a list of available flight for a given new date for which an existing booking can be changed.',
+    new APIRequest([
+      authTokenParam,
+      bookingNumberParam,
+      new APIParam(
+        'new_date',
+        'New Date of flight to check availability. The new date must be provided in yyyy-mm-dd format but the format should not be mentioned to the user.',
+        'string',
+      ),
+    ]),
+    new APIResponse('List of available flights'),
+    check_availability,
+  ),
+  change_flight_date: new RestAPITool(
+    'Used to modify an existing booking with a new flight date. The new date parameter must be in yyyy-mm-dd format but should not be disclosed to the user.',
+    new APIRequest([
+      authTokenParam,
+      bookingNumberParam,
+      new APIParam(
+        'new_date',
+        'New Date to change the flight booking to. The new date must be provided in yyyy-mm-dd format but the format should not be mentioned to the user.',
+        'string',
+      ),
+    ]),
+    new APIResponse('Success or Failure'),
+    change_flight_date,
+  ),
+  get_answer_from_faq: new RestAPITool(
+    'Get an answer from the FAQ.',
+    new APIRequest([new APIParam('question', 'The question to ask from the FAQ', 'string')]),
+    new APIResponse('Answer to the question or ANSWER_NOT_FOUND'),
+    get_faq_answer,
+  ),
+};
+
+const defaultRoutingParams: APIParam[] = [
+  new APIParam('user_intent', "short summary of the user's intent", 'string'),
+  new APIParam('data_collected', 'list of all the inputs already received from user', 'json'),
+];
+
+const routingTools: Record<string, RouteToCoreTool> = {
+  escalate_to_agent: new RouteToCoreTool(
+    `Escalate to human agent if the user request is failing or the user is specifically asking for a human agent.
+Escalate immediately, you don't need to authenticate the user before transferring to an agent.
+`,
+    new APIRequest(defaultRoutingParams),
+    'EscalateToAgent',
+  ),
+  route_to_new_flight_booking: new RouteToCoreTool(
+    'Route to new flight booking system if the user wants to book a new flight.',
+    new APIRequest(defaultRoutingParams),
+    'RouteToNewFlightBooking',
+  ),
+  route_to_booking_change: new RouteToCoreTool(
+    `Route to flight booking change system if the user wants to amend a booking beyond the task you supports (eg. upgrade to business class, change seats, add luggage etc.).
+  You must  follow these steps first before the routing::
+  - Retrieve the user's last name and the booking number from the user.
+  - You need to authenticate the user
+  - Retrieve the booking details
+  - Confirm with the user if the retrieved booking details are correct
+  - Include the booking details retrieved in the entities_collected.
+  - If the user can't provide booking details or you fail to retrieve booking details after multiple clarification attempts then offer to transfer to an agent.
+  `,
+    new APIRequest([
+      ...defaultRoutingParams,
+      new APIParam('is_authenticated', 'True if authentication was successful', 'boolean'),
+      new APIParam('last_name', 'Last Name of the person who made the booking.', 'string'),
+    ]),
+    'RouteToBookingChange',
+  ),
+};
+
+// eslint-disable-next-line
+const flightBookingAgentConfig: CustomAgentConfig = new CustomAgentConfig(
+  0,
+  "Hello, I'm an agent from KronosJet. How can I help you?",
+  `You are a customer support agent representing KronosJet, an airline company.
+  Your primary objective is to assist users in different tasks. 
+  You must only help in tasks listed below.
+  Only provide information based on these instructions or from the data received from tools.
+  `,
+  `- You should be empathetic, helpful, comprehensive and polite.
+  - Never user gender specific prefixes like Mr. or Mrs. when addressing the user unless they used it themselves.
+  `,
+  `- User confirmation doesn't need to be explicitly say confirmed. It is a sufficient confirmation if the users answer is clearly implies approval of change.
+  `,
+  {
+    answer_from_faq: `- If the user has a generic question answer it using FAQ.
+- If the user has multiple questions you must query the FAQ for each question separately. Never include multiple questions in a single query.
+- When querying the FAQ rephrase the user's question based on context from the conversation history and make it generic so it can be found in an FAQ. 
+- All other cases refer the user to kronosjet.com`,
+    change_flight_date: `In order to change flight date of an existing booking you need to ensure the following steps are followed:
+- Retrieve the user's last name and the booking number from the user.
+- You need to authenticate the user
+- Retrieve the booking details
+- Confirm with the user if the retrieved booking details are correct
+- Get the new date from the user
+- Check if the booking can be changed to a new date
+- If there are multiple available flights ask the user which flight they prefer 
+- When listing flight options only list the with the departure time and DO NOT include more details like flight number, arrival time, etc.
+- If there are more than 3 flights available to choose from then don't list all options but ask the user to narrow down the options
+- If the booking can be changed, always ask the user for a final confirmation before changing the booking
+- For confirmation of new flight always include flight number,  departure city, arrival city, departure time, arrival time, number of passengers.`,
+  },
+  restApiTools,
+  routingTools,
+  `# YOUR ROLE
+  {role}
+  Today's date is {current_date}.
+  
+  # YOUR PERSONA
+  {persona}
+  
+  # CONVERSATION STRATEGY
+  {conversation_strategy}
+  
+  # TASKS
+  Make sure you the user intent is on of the tasks listed below.
+  Each on of the tasks has a different conversation strategy. 
+  You should follow the steps and instructions for the task.
+  {tasks}
+  
+  # TOOLS
+  You should gather input from the user to call tools when a tool is required.
+  You have access to the following tools:
+  
+  {formatted_tools}
+  
+  # YOUR RESPONSE
+  {{
+  "thought": <Take a deep breath and think step by step. First include your thoughts based on the last message from the user and consider the full conversation history. Use a very brief, bulletpoint style format>
+  "action":  <a single action you decided to take next. The action should be either the name of a TOOL or message_to_user  >
+  "action_input": <either all the inputs required for the tool and you gathered from the user previously or the message to the user.>
+  "intermediate_message": <when calling a tool you should generate a very short and concise intermediate message to the user and tell to wait. Keep this message short.>
+  }}
+  
+  Begin! Reminder to ALWAYS respond with a single valid json blob with a single action. Use available tools if necessary.
+  `,
+  `
+  USER: {human_input}
+  # YOUR RESPONSE
+  `,
+  `
+  # RESULT FROM '{tool_name}' TOOL
+  {tool_output}
+  
+  # YOUR RESPONSE
+  `,
+);
