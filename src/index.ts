1c2cdc5973a0f0a0c49463e20f2d30d20c752884
Ivan Stoyanov <ge36qij@mytum.de>
2023-11-14 00:21:35 +0200

    added finalized API documentation

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ dotenv.config(); // Load environment variables from .env file
 
 import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 
 // routers
 import simulationRouter from '../src/simulation/router/simulation.router';
@@ -11,11 +13,15 @@ import authRouter from './simulation/router/auth.router';
 import dashRouter from './simulation/router/dashboard.router';
 import llmRouter from './simulation/router/llms.router';
 
+import { swaggerOptions } from './simulation/docs/swagger.options';
+
+const specs = swaggerJsdoc(swaggerOptions);
 const app = express();
 const port = process.env.NODE_DOCKER_PORT || 3000;
 
 app.use(bodyParser.json());
 
+app.use('/api/v1/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 app.use('/api/v1/simulation', simulationRouter);
 app.use('api/v1/chat', chatRouter);
 
