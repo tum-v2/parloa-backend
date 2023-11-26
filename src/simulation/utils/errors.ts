01df97e85eb4c58a5a8b880f245c9987afdad9ac
Ivan Stoyanov <ge36qij@mytum.de>
2023-11-26 11:16:49 +0200

    Added documentation, fixed linting and README

--- src/simulation/utils/errors.ts
+++ src/simulation/utils/errors.ts
@@ -1,3 +1,9 @@
+/**
+ * Returns response for Internal Error
+ * @param error - Raised error
+ * @param updates - The updates to apply to the simulation object.
+ * @returns An object containing code, message and details
+ */
 function INTERNAL_SERVER_ERROR(error: unknown) {
   return {
     code: 'INTERNAL_SERVER_ERROR',
