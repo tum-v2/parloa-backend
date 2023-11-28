c6cc828b2458ee98e699bee8180db6a2575984a0
Tan-Colin Wei <tan-colin.wei@tum.de>
2023-11-28 16:10:35 +0900

    Add route

--- src/evaluation/router/evaluation.router.ts
+++ src/evaluation/router/evaluation.router.ts
@@ -1,1 +1,12 @@
 // Router that routes incoming requests to relevant controller function
+import express from 'express';
+const router = express.Router();
+
+import evaluationController from '../api/evaluation.controller';
+
+// region POST //
+router.post('/run', evaluationController.run);
+// endregion POST //
+
+// region GET //
+// router.get();
