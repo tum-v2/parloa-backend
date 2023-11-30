012c4a939650a86c3888b98842d2605d97ddcb0c
Fabian <ge58wod@mytum.de>
2023-11-30 23:07:37 +0100

    adjust interface of conversation model

--- src/simulation/db/models/conversation.model.ts
+++ src/simulation/db/models/conversation.model.ts
@@ -1,9 +1,9 @@
 import { Schema, Document, model, Types } from 'mongoose';
-import { MessageModel } from './message.model';
 import { ConversationStatus } from '../enum/enums';
+import { MessageDocument } from './message.model';
 
 interface ConversationDocument extends Document {
-  messages: Types.ObjectId[] | (typeof MessageModel)[];
+  messages: Types.ObjectId[] | MessageDocument[];
   startTime: Date;
   status: ConversationStatus;
   usedEndpoints: string[];
