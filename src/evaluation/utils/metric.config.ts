375b9a9ea554c344084198ae602b4599f7227e08
MarekLorenz <marek.lorenz@outlook.de>
2023-12-04 01:07:25 +0100

    add metrics in enum, update weights

--- src/evaluation/utils/metric.config.ts
+++ src/evaluation/utils/metric.config.ts
@@ -4,12 +4,18 @@ enum MetricNameEnum {
   SUCCESS = 'success',
   RESPONSE_TIME = 'response_time',
   MESSAGE_COUNT = 'message_count',
+  RECOVERY_RATE = 'recovery_rate',
+  SIMILARITY = 'similarity',
+  SENTIMENT_ANALYSIS = 'sentiment_analysis',
 }
 
 const metricWeightMap: Map<MetricNameEnum, number> = new Map([
-  [MetricNameEnum.SUCCESS, 1 / 3],
-  [MetricNameEnum.RESPONSE_TIME, 1 / 3],
-  [MetricNameEnum.MESSAGE_COUNT, 1 / 3],
+  [MetricNameEnum.SUCCESS, 1 / 6],
+  [MetricNameEnum.RESPONSE_TIME, 1 / 6],
+  [MetricNameEnum.MESSAGE_COUNT, 1 / 6],
+  [MetricNameEnum.RECOVERY_RATE, 1 / 6],
+  [MetricNameEnum.SIMILARITY, 1 / 6],
+  [MetricNameEnum.SENTIMENT_ANALYSIS, 1 / 6],
 ]);
 
 export { MetricNameEnum, metricWeightMap };
