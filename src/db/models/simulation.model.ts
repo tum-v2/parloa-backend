287c2a526222ffec74f3695a8f6d21609256c317
Ivan Stoyanov <ge36qij@mytum.de>
2023-12-10 10:31:13 +0200

    Cascade delete simulations

--- src/db/models/simulation.model.ts
+++ src/db/models/simulation.model.ts
@@ -1,11 +1,12 @@
 import { Schema, Document, model, Types } from 'mongoose';
 import { AgentDocument } from '@db/models/agent.model';
-import { ConversationDocument } from '@db/models/conversation.model';
+import { ConversationDocument, ConversationModel } from '@db/models/conversation.model';
 import { SimulationType } from '@enums/simulation-type.enum';
-import { EvaluationDocument } from '@db/models/evaluation.model';
-import { OptimizationDocument } from '@db/models/optimization.model';
+import { EvaluationDocument, EvaluationModel } from '@db/models/evaluation.model';
+import { OptimizationDocument, OptimizationModel } from '@db/models/optimization.model';
 import { SimulationScenario } from '@enums/simulation-scenario.enum';
 import { SimulationStatus } from '@enums/simulation-status.enum';
+import { CallbackError } from 'mongoose';
 
 interface SimulationDocument extends Document {
   scenario: SimulationScenario;
@@ -44,6 +45,26 @@ const SimulationSchema: Schema = new Schema(
     timestamps: true,
   },
 );
+
+SimulationSchema.pre<SimulationDocument>('findOneAndDelete', async function (next) {
+  try {
+    const simulation = await SimulationModel.findOne(this.getFilter()).exec();
+    await ConversationModel.deleteMany({ _id: { $in: simulation?.conversations } }).exec();
+    if (simulation?.evaluation) {
+      await EvaluationModel.findByIdAndDelete(simulation.evaluation).exec();
+    }
+    if (simulation?.optimization) {
+      await OptimizationModel.findByIdAndDelete(simulation.optimization).exec();
+    }
+
+    await SimulationModel.updateMany({ abPartner: simulation?._id }, { $unset: { abPartner: 1 } });
+
+    next();
+  } catch (error) {
+    console.log(error);
+    next(error as CallbackError);
+  }
+});
 
 const SimulationModel = model<SimulationDocument>('Simulation', SimulationSchema);
 
