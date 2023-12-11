10d3f3e54126ed2d6b6db303327c206368d37e71
Ivan Stoyanov <ge36qij@mytum.de>
2023-12-11 20:00:35 +0200

    Fix jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,4 @@
 // eslint-disable-next-line tsdoc/syntax
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 // eslint-disable-next-line no-undef
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-};
+export default {};
