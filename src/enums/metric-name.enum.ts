7a82f64a911e71d0eefcf7327c9aa47862065f4b
Fabi987 <ge58wod@mytum.de>
2023-12-16 09:09:21 +0100

    evaluation enum: fix RESPONSE_TIME missing

--- src/enums/metric-name.enum.ts
+++ src/enums/metric-name.enum.ts
@@ -1,6 +1,7 @@
 enum MetricNameEnum {
   SUCCESS = 'success',
   MESSAGE_COUNT = 'message_count',
+  RESPONSE_TIME = 'response_time',
   RECOVERY_RATE = 'recovery_rate',
   SIMILARITY = 'similarity',
   SENTIMENT_ANALYSIS = 'sentiment_analysis',
