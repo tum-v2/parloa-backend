e37632200e1527475183324d56c6f5f876595c1a
Marc <ge42ruv@mytum.de>
2023-12-16 19:13:50 +0100

    test agent api

--- __tests__/agent.test.ts
+++ __tests__/agent.test.ts
@@ -1,46 +1,98 @@
 import request, { Response } from 'supertest';
-import { app } from '../src/index';
-//import { disconnectFromDatabase } from '@db/config/db.config';
 
 const validInput = {
   name: 'TEST AGENT',
   llm: 'FAKE',
   temperature: 0,
-  maxTokens: 250,
+  maxTokens: 256,
   domain: 'FLIGHT',
-  prompt: 'concise',
-  temporary: true,
+  prompt: 'sarcastic',
 };
 
-const invalidInput = {
-  name: 'TEST AGENT',
-  llm: 'FAKE',
-  temperature: 0,
-  maxTokens: 250,
-  domain: 'FLIGHT',
-  prompt: 'concise',
-  temporary: true,
-};
+let agentId = '';
 
 describe('POST /api/v1/agent/', () => {
   let validResponse: Response;
-  let invalidResponse: Response;
 
   beforeEach(async () => {
-    validResponse = await request(app).post('/api/v1/agent/').send(validInput);
-    invalidResponse = await request(app).post('/api/v1/agent/').send(invalidInput);
+    validResponse = await request('http://localhost:3000').post('/api/v1/agent/').send(validInput);
+    agentId = validResponse.body._id;
   });
 
   afterEach(() => {
     validResponse = {} as Response;
-    invalidResponse = {} as Response;
   });
 
   it('should return 201 for valid input', () => {
     expect(validResponse.status).toBe(201);
   });
+});
 
-  it('should return 400 for invalid input', () => {
-    expect(invalidResponse.status).toBe(400);
+describe('GET /api/v1/agent/:id', () => {
+  let validResponse: Response;
+
+  beforeEach(async () => {
+    validResponse = await request('http://localhost:3000').get(`/api/v1/agent/${agentId}`);
+  });
+
+  afterEach(() => {
+    validResponse = {} as Response;
+  });
+
+  it('should return 200 for valid agent ID', () => {
+    expect(validResponse.status).toBe(200);
+  });
+});
+
+describe('GET /api/v1/agent/all', () => {
+  let validResponse: Response;
+
+  beforeEach(async () => {
+    validResponse = await request('http://localhost:3000').get(`/api/v1/agent/all`);
+  });
+
+  afterEach(() => {
+    validResponse = {} as Response;
+  });
+
+  it('should return 200 for valid agent ID and return at least 1 agent', () => {
+    expect(validResponse.status).toBe(200);
+    expect(validResponse.body.length).toBeGreaterThan(0);
+  });
+});
+
+describe('PUT /api/v1/agent/:id', () => {
+  let validResponse: Response;
+
+  beforeEach(async () => {
+    validResponse = await request('http://localhost:3000').put(`/api/v1/agent/${agentId}`).send({ prompt: 'nonative' });
+  });
+
+  afterEach(() => {
+    validResponse = {} as Response;
+  });
+
+  it('should return 200 for valid agent ID and return agent with updated prompt', () => {
+    expect(validResponse.status).toBe(200);
+    expect(validResponse.body.prompt).toBe('nonative');
+  });
+});
+
+describe('DELETE /api/v1/agent/:id', () => {
+  let validResponse: Response;
+  let getResponse: Response;
+
+  beforeEach(async () => {
+    validResponse = await request('http://localhost:3000').delete(`/api/v1/agent/${agentId}`);
+    getResponse = await request('http://localhost:3000').get(`/api/v1/agent/${agentId}`);
+  });
+
+  afterEach(() => {
+    validResponse = {} as Response;
+  });
+
+  it('should return 204 for valid agent ID and agent should be deleted', () => {
+    expect(validResponse.status).toBe(204);
+    expect(getResponse.status).toBe(404);
   });
 });
