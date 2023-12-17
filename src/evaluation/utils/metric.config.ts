a05442f03c94d7e77637ad2bcbdff5b1b46b1ae1
MarekLorenz <marek.lorenz@outlook.de>
2023-12-17 21:23:53 +0100

    change weights

--- src/evaluation/utils/metric.config.ts
+++ src/evaluation/utils/metric.config.ts
@@ -1,12 +1,12 @@
 import { MetricNameEnum } from '@enums/metric-name.enum';
 
 const metricWeightMap: Map<MetricNameEnum, number> = new Map([
-  [MetricNameEnum.SUCCESS, 1 / 6],
-  [MetricNameEnum.RESPONSE_TIME, 1 / 6],
-  [MetricNameEnum.MESSAGE_COUNT, 1 / 6],
-  [MetricNameEnum.RECOVERY_RATE, 1 / 6],
-  [MetricNameEnum.SIMILARITY, 1 / 6],
-  [MetricNameEnum.SENTIMENT_ANALYSIS, 1 / 6],
+  [MetricNameEnum.SUCCESS, 0.2],
+  [MetricNameEnum.RESPONSE_TIME, 0.2],
+  [MetricNameEnum.MESSAGE_COUNT, 0.2],
+  [MetricNameEnum.RECOVERY_RATE, 0.1],
+  [MetricNameEnum.SIMILARITY, 0.1],
+  [MetricNameEnum.SENTIMENT_ANALYSIS, 0.2],
 ]);
 
 export { metricWeightMap };
